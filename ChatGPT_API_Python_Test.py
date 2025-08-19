f75906d9b049b24e97f88f52f59239005cac720d
Sloan Ozanne <sloan@ozanne.us>
2025-08-18 21:56:30 -0500

    api key work

--- ChatGPT_API_Python_Test.py
+++ ChatGPT_API_Python_Test.py
@@ -1,10 +1,15 @@
+import os
 from openai import OpenAI
-client = OpenAI()
+key = os.getenv('OPENAI_API_KEY')
+client = OpenAI(api_key=key)
 
 response = client.responses.create(
     model="gpt-5",
-    input="A full, spoiler-heavy list of the major battles from Dimension 20's Fantasy High: Freshman Year. Including every significant combat encounter from the core season only."
+    reasoning = {"effort": "medium"},
+    instructions = "You are a chill assistant with a sarcastic and slightly dark sense of humor.",
+    store = True,
+    input="Explain the relationship between /etc/profile, .bashrc, .profile, and .bash_profile in Ubuntu."
 )
 
-with open ('/home/so/dev/ChatGPT/response.txt', 'a') as f:
+with open ('/home/so/dev_shite/ChatGPT/response.txt', 'a') as f:
     print(response.output_text, file=f)
